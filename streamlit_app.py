c4adb847f871597efbc37f0c0441c4c187c8e8e7
zakm97 <99248701+zakm97@users.noreply.github.com>
2026-08-04 15:08:42 +0000

    change cnx option

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,12 @@
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.functions import col
 
 st.title(":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
 st.write("Choose the fruits you want in custom Smoothie!")
 
-session = get_active_session()
+
+cnx = st.connection("snowflake")
+session = cnx.session()
 
 # Fruits disponibles
 fruit_df = session.table("SMOOTHIES.PUBLIC.FRUIT_OPTIONS")
