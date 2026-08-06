01c57ae733cf206b074d531a32bad1ec7ff87fa6
zakm97 <99248701+zakm97@users.noreply.github.com>
2026-08-06 09:40:47 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,4 +39,4 @@ if ingredients_list and name_on_order:
 
 import requests  
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
