d8f761932010140ea5ca7df394f6154b6d362247
zakm97 <99248701+zakm97@users.noreply.github.com>
2026-08-06 09:34:39 +0200

    Integrate Smoothiefroot API for watermelon data
    Added API call to fetch watermelon data and display it.

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,3 +36,7 @@ if ingredients_list and name_on_order:
         """
         session.sql(my_insert_stmt).collect()
         st.success("Your Smoothie is ordered!", icon=":material/thumb_up:")
+
+import requests  
+smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+st.text(smoothiefroot_response)
