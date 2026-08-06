670df1b543ed7b087bdc5b1140cba4d321485e83
zakm97 <99248701+zakm97@users.noreply.github.com>
2026-08-06 09:42:16 +0200

    Comment out smoothie response display
    Comment out the line displaying the smoothie response.

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,7 +39,7 @@ if ingredients_list and name_on_order:
 
 import requests  
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
+#st.text(smoothiefroot_response.json())
 
 
 sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width = True)
